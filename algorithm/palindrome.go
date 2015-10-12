f741e0bd731c098a9ae7038f44913556945273a0
Thiago Augustus <thiagoaugustusdeoliveira@gmail.com>
2015-10-12 00:35:46 -0300

    Created a function that check if an word is a palindrome

--- algorithm/palindrome.go
+++ algorithm/palindrome.go
@@ -1,6 +1,11 @@
 package algorithm
 
 // Palindrome function
-func Palindrome(text string) bool {
-	return true
+func Palindrome(text []string) bool {
+	input := text[0]
+	var output string
+	for k := range input {
+		output = input[k:k+1] + output
+	}
+	return input == output
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/thiagoaugustus/GoLang-Palindrome/algorithm"
 	"os"
 )
 
@@ -9,5 +10,10 @@ import (
 func main() {
 	// Read first user input string
 	input := os.Args[1:2]
-	fmt.Println(input)
+	fmt.Printf("Is '%s' a palindrome?\n", input[0])
+	if algorithm.Palindrome(input) {
+		fmt.Println("Yes, it is!")
+	} else {
+		fmt.Println("No, it isn't!")
+	}
 }
